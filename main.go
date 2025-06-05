8f1cb98a02869ae5b4ecc9bf1576d86d4a172699
bhavya418 <shahb3703@gmail.com>
2025-06-05 10:40:49 +0530

    build change

--- main.go
+++ main.go
@@ -31,10 +31,9 @@ func logHandler(severity string) http.HandlerFunc {
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	
+	port := "8080"
+
 
 	http.HandleFunc("/info", logHandler("INFO"))
 	http.HandleFunc("/warn", logHandler("WARNING"))
